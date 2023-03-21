07968e90f9cf82f11640d45214929f052b1ef03f
Lecser <107482432+Lecser@users.noreply.github.com>
2023-03-21 10:09:31 +0300

    add babel loader on webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,16 +1,18 @@
 import type webpack from 'webpack';
 
-import {buildCSSLoader} from './loaders/buildCSSLoader';
-import {buildImageLoader} from './loaders/buildImageLoader';
-import {buildSvgrLoader} from './loaders/buildSvgrLoader';
-import {buildTypeScriptLoader} from './loaders/buildTypeScriptLoader';
-import {type BuildOptions} from './types/config';
+import { buildBabelLoader } from './loaders/buildBabelLoader';
+import { buildCSSLoader } from './loaders/buildCSSLoader';
+import { buildImageLoader } from './loaders/buildImageLoader';
+import { buildSvgrLoader } from './loaders/buildSvgrLoader';
+import { buildTypeScriptLoader } from './loaders/buildTypeScriptLoader';
+import { type BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
   return [
     buildSvgrLoader(),
     buildImageLoader(),
     buildCSSLoader(isDev),
+    buildBabelLoader(isDev),
     buildTypeScriptLoader()
   ];
 }
